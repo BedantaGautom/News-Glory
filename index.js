0d8df5f31816976fb51c3126398c8bfea6fcfba2
Bedanta Gautom <bedantagautom2001@gmail.com>
2023-10-16 12:00:48 +0000

    Updated key

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const newsdetails = document.getElementById("newsdetails");
 var newsDataArr = [];
 
 // apis 
-const API_KEY = "60c06fa8db85420699cd776056ffc53e";
+const API_KEY = "e8b9b587e68844c09045877fbf09f1b5";
 const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
 const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=";
 const BUSINESS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=";
